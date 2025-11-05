8d90512ccd3d41de64d93aabf7d0976d61558f1a
Har D <madhairsilence@gmail.com>
2025-11-05 17:39:36 +0530

    Add URL request handling for popup windows

---
+++ wl-script.js
@@ -0,0 +1,28 @@
+function handleUrlRequests(){
+    var w = 800, h = 600;
+        var left = Math.max((screen.width - w) / 2, 0);
+        var top = Math.max((screen.height - h) / 2, 0);
+            var features = 'noopener,noreferrer,' +
+        'toolbar=no,location=no,status=no,menubar=no,' +
+        'scrollbars=yes,resizable=yes,' +
+        'width=' + w + ',height=' + h + ',top=' + top + ',left=' + left;
+
+
+    $('h4 a').click(function(e){                          
+    clickCount++;                                    
+    e.preventDefault(); 
+    var popupUrl = 'popup.html';
+    var popupName = 'popupWindow';
+    var articleTitle = $.trim($(this).text());
+    var articleUrl = $(this).attr('href');
+    var popupWithParams = popupUrl + '?clickCount=' + encodeURIComponent(clickCount) + '&url=' + encodeURIComponent(articleUrl);
+    if( clickCount >= 3 ){
+        window.open(popupWithParams, popupName, features);
+    }
+    else{
+        window.open(articleUrl, popupName, features);
+    }
+    
+    
+});
+}
